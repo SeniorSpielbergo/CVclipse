189da78579cc7d75f8e1ef6b394da09d297b7ed9
Sven Schuster <s.schuster@tu-bs.de>
2014-01-02 10:40:32 +0100

    slight changes

--- trunk/de.tu_bs.cs.isf.mbse.cvclipse/model/cvclipse.cs
+++ trunk/de.tu_bs.cs.isf.mbse.cvclipse/model/cvclipse.cs
@@ -17,46 +17,57 @@ RULES {
 					("place" ":" place[TEXT])?
 					("date" ":" date)
 					("personal" personalInformation)
-					(letter? cv);
+					(letter | cv)*; // letter?
 
 	PersonalInformation ::= "personal"
-							("firstname" ":" firstname[TEXT])
-							("surname" ":" surname[TEXT])
-							("birthname" ":" birthName[TEXT])?
-							("birthplace" ":" birthplace[TEXT])
-							("birthdate" ":" birthdate)
-							("marital status" ":" maritalStatus)?
-							("nationality" ":" nationality)?
-							("street" ":" street[TEXT])
-							("city" ":" city[TEXT])
-							("country" ":" country)?
-							("email" ":" email[EMAIL])?
-							("phone" ":" phone[TEXT])?
-							("mobile" ":" mobile[TEXT])?
-							("fax" ":" fax[TEXT])?
-							("website" ":" website[TEXT])?
-							("github" ":" github[TEXT])?
-							("linkedin" ":" linkedIn[TEXT])?
-							("twitter" ":" twitter[TWITTER])?;
+							(("firstname" ":" firstname[TEXT])
+							("surname" ":" surname[TEXT])			|
+							("birthname" ":" birthName[TEXT]) 		| // ?
+							("birthplace" ":" birthplace[TEXT])		|
+							("birthdate" ":" birthdate)				|
+							("marital status" ":" maritalStatus) 	| // ?
+							("nationality" ":" nationality) 		| // ?
+							("street" ":" street[TEXT])				|
+							("city" ":" city[TEXT])					|
+							("country" ":" country) 				| // ?
+							("email" ":" email[EMAIL]) 				| // ?
+							("phone" ":" phone[TEXT]) 				| // ?
+							("mobile" ":" mobile[TEXT]) 			| // ?
+							("fax" ":" fax[TEXT])					| // ?
+							("website" ":" website[TEXT])			| // ?
+							("github" ":" github[TEXT])				| // ?
+							("linkedin" ":" linkedIn[TEXT])			| // ?
+							("twitter" ":" twitter[TWITTER]) 		  // ?
+							)*;
 							
 	Letter ::= "letter"
 				"recipient"
-				("company" ":" recipientCompany[TEXT])
-				("street" ":" recipientStreet[TEXT])
-				("city" ":" recipientCity[TEXT])
-				("country" ":" recipientCountry[TEXT])?
-				("attention" ":" recipientAttention[TEXT])?
-				("opening" ":" opening)?
-				("text" ":" text)
-				("closing" ":" closing)?
-				("enclosure" ":" enclosure)?;
+				(("company" ":" recipientCompany[TEXT])				|
+				("street" ":" recipientStreet[TEXT])				|
+				("city" ":" recipientCity[TEXT])					|
+				("country" ":" recipientCountry[TEXT])				| // ?
+				("attention" ":" recipientAttention[TEXT])			| // ?
+				("opening" ":" opening)								| // ?
+				("text" ":" text)									|
+				("closing" ":" closing)								| // ?
+				("enclosure" ":" enclosure)							  // ?
+				)*;
 				
 	CV ::= "cv"
 			"picture" ":" picture[TEXT]?
 			blocks+;
+	
+	ItemBlock ::= "block"
+					"title" ":" title
+					(items)+;
+					
+	TextItem ::= ((leftContent) ("," leftContent)*)? "|" rightContent;
+	DateItem ::= begin ("-" end)? "|" rightContent;
 			
 	Date ::= year[FOUR_DIGIT] "/" month[TWO_DIGIT] ("/" day[TWO_DIGIT])?;
 	
-	LanguageToTextMapEntry ::= "bla";
-	ItemBlock ::= "block";
+	LanguageToTextMapEntry ::= key[GERMAN : "german", ENGLISH : "english", FRENCH : "french", SPANISH : "spanish"] ":" value;
+	
+	Text ::= content[TEXT];
+	
 }
