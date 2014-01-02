de5ad6f4721fcbc53c187479090515ec20efcac4
David Wille <david.wille@ewetel.net>
2014-01-02 18:44:37 +0100

    console

---
+++ trunk/de.tu_bs.cs.isf.mbse.cvclipse.generator/src/de/tu_bs/cs/isf/mbse/cvclipse/generator/Console.java
@@ -0,0 +1,132 @@
+package de.tu_bs.cs.isf.mbse.cvclipse.generator;
+
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+
+public class Console {
+	private static boolean enabledDebug;
+	private static boolean enabledWarning;
+	private static boolean disablePluginConsole = false;
+	
+	public static boolean isEnabledDebug() {
+		return enabledDebug;
+	}
+
+	public static void setEnabledDebug(boolean enabled) {
+		enabledDebug = enabled;
+	}
+
+	public static boolean isEnabledWarning() {
+		return enabledWarning;
+	}
+
+	public static void setEnabledWarning(boolean enabled) {
+		enabledWarning = enabled;
+	}
+
+	public static void disablePluginConsole(boolean bool){
+		disablePluginConsole = bool;
+	}
+	
+	public static final String CONSOLE = "familymining_output_console";
+
+	// http://wiki.eclipse.org/FAQ_How_do_I_write_to_the_console_from_a_plug-in%3F
+	private static MessageConsole getConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++)
+			if (name.equals(existing[i].getName()))
+				return (MessageConsole) existing[i];
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
+
+	public static MessageConsole getConsole(){
+		return getConsole(CONSOLE);
+	}
+	
+	public static void println(String message) {
+		if(disablePluginConsole)
+			System.out.println(message);
+		else
+			getConsole().newMessageStream().println(message);
+	}
+	
+	public static void println() {
+		if(disablePluginConsole)
+			System.out.println();
+		else
+			getConsole().newMessageStream().println();
+	}
+
+	public static void print(String message) {
+		if(disablePluginConsole)
+			System.out.print(message);
+		else
+			getConsole().newMessageStream().print(message);
+	}
+	
+	public static void debugln(String message) {
+		if(isEnabledDebug()){
+			if(disablePluginConsole)
+				System.out.println(message);
+			else
+				getConsole().newMessageStream().println(message);
+		}
+	}
+	
+	public static void warningln(String message) {
+		if(isEnabledWarning()){
+			if(disablePluginConsole)
+				System.out.println(message);
+			else
+				getConsole().newMessageStream().println(message);
+		}
+	}
+	
+	public static void debugln() {
+		if(isEnabledDebug()){
+			if(disablePluginConsole)
+				System.out.println();
+			else
+				getConsole().newMessageStream().println();
+		}
+	}
+	
+	public static void warningln() {
+		if(isEnabledWarning()){
+			if(disablePluginConsole)
+				System.out.println();
+			else
+				getConsole().newMessageStream().println();
+		}
+	}
+	
+	public static void debug(String message) {
+		if(isEnabledDebug()){
+			if(disablePluginConsole)
+				System.out.print(message);
+			else
+				getConsole().newMessageStream().print(message);
+		}
+	}
+	
+	public static void warning(String message) {
+		if(isEnabledWarning()){
+			if(disablePluginConsole)
+				System.out.print(message);
+			else
+				getConsole().newMessageStream().print(message);
+		}
+	}
+	
+	public static void clear() {
+		getConsole().clearConsole();
+	}
+	
+}
--- trunk/de.tu_bs.cs.isf.mbse.cvclipse.generator/src/de/tu_bs/cs/isf/mbse/cvclipse/generator/RunHandler.java
+++ trunk/de.tu_bs.cs.isf.mbse.cvclipse.generator/src/de/tu_bs/cs/isf/mbse/cvclipse/generator/RunHandler.java
@@ -10,16 +10,22 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleView;
 import org.eclipse.ui.handlers.HandlerUtil;
-
-import de.tu_bs.cs.isf.mbse.cvclipse.M2T;
 
 public class RunHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		showConsole();
+		Console.clear();
 		List<String> inputs = new LinkedList<String>();
 		
 		IStructuredSelection selection = (IStructuredSelection) PlatformUI.getWorkbench()
@@ -57,6 +63,25 @@ public class RunHandler extends AbstractHandler {
 				window.getShell(),
 				"An error occured!",
 				message);
+	}
+	
+	private void showConsole() {
+		IWorkbench wb = PlatformUI.getWorkbench();
+
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+
+		IConsole myConsole = Console.getConsole();
+		String id = IConsoleConstants.ID_CONSOLE_VIEW;
+		IConsoleView view;
+		try {
+			view = (IConsoleView) page.showView(id);
+			view.display(myConsole);
+		}
+		catch (PartInitException e1) {
+			e1.printStackTrace();
+			return;
+		}
 	}
 
 }
